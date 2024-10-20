0aec592c805471736db8e5732e8e207160d87124
Andrew Jones <33326259+jonesyriffic@users.noreply.github.com>
2024-10-20 17:44:41 +0100

    Footer added. Contact Form built.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
 import { Analytics } from '@vercel/analytics/react';
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
     <>
       <Hero />
       <Projects />
+      {/*Need to hook contact form up - see other tutorial for this
+      <Contact />*/}
+      <Footer />
+      <Analytics />
     </>
   );
 }
